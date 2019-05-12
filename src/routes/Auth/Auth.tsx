e91c97b2ad8233ef09b2e4ac03b770ed5e271bc7
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-05-12 17:55:54 -0400

    Login and Signup routes

--- src/routes/Auth/Auth.tsx
+++ src/routes/Auth/Auth.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Route } from "react-router-dom";
+
+import {Login, Signup} from './components'
+
 function Auth() {
-    return <>Auth</>
+    return <>
+        <Route path='/login' exact component={Login}/>
+        <Route path='/signup' exact component={Signup}/>
+    </>
 }
 
 export default Auth
