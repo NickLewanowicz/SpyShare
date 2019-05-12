008c708fa0b3632127a7a0c8b74b4481b78f7c13
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-05-12 19:07:45 -0400

    init base components export

---
+++ src/components/Firebase/index.ts
@@ -0,0 +1,6 @@
+import {FirebaseContext, useFirebase} from './context';
+import {FirebaseProvider} from './provider'
+import Firebase from './firebase';
+
+
+export { Firebase, FirebaseContext, FirebaseProvider, useFirebase };
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+import { Firebase, FirebaseContext, FirebaseProvider, useFirebase } from './Firebase';
+
+export {Firebase, FirebaseContext, FirebaseProvider, useFirebase}
