c1fd54ee12e613249175cd906b516f0d1d07bd8a
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-05-12 19:07:57 -0400

    Wrap app in provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import {Auth, Dashboard} from './routes'
+import {Auth, Dashboard} from './routes';
+import {FirebaseProvider} from './components';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/" component={Auth} />
-      </Switch>
-    </Router>
+    <FirebaseProvider>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/" component={Auth} />
+        </Switch>
+      </Router>
+    </FirebaseProvider>
   );
 }
 
