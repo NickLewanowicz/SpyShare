b73d06ff04d03b4b11eb14ecb85e9088906f3975
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-07-06 14:57:55 -0400

    init login

--- src/routes/Auth/components/Login/Login.tsx
+++ src/routes/Auth/components/Login/Login.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
-function Login() {
-    return <>Login</>
-}
+import useReactRouter from 'use-react-router';
 
-export default Login
+import {Button} from 'antd'
+import {useFirebase} from '../../../../components'
+
+export function Login() {
+    const firebase = useFirebase()
+    const { history } = useReactRouter();
+    if (!firebase) {
+        return <>Error</>
+    }
+    const doSignIn = async () => {
+        await firebase.doSignInWithGoogle()
+        if (firebase.auth.currentUser) {
+            history.push('/register')
+        }
+        firebase.auth.currentUser && console.log(firebase.auth.currentUser.photoURL)
+    }
+
+    const doSignout = async () => {
+         const response = await firebase.doSignOut()
+         console.log(response)
+    }
+
+    const profile = firebase.auth.currentUser && firebase.auth.currentUser.photoURL;
+    return <>
+        Login
+        <Button onClick={doSignIn}/><Button onClick={doSignout}/>
+    </>
+}
