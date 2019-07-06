2debed746c5e4ad4dba309f3884bb9a44e4ba1a8
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-07-06 14:57:10 -0400

    add register route

--- src/routes/Auth/Auth.tsx
+++ src/routes/Auth/Auth.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import { Route } from "react-router-dom";
 
-import {Login, Signup} from './components'
+import {Login, Register, Signup} from './components'
 
 function Auth() {
     return <>
         <Route path='/login' exact component={Login}/>
         <Route path='/signup' exact component={Signup}/>
+        <Route path='/register' exact component={Register}/>
     </>
 }
 
