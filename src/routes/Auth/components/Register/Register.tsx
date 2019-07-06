b624acf4410424b5f2944cb2176e891518968d66
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-07-06 14:58:14 -0400

    Register form template

---
+++ src/routes/Auth/components/Register/Register.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+import {
+  useForm,
+  useField,
+  submitFail,
+  submitSuccess
+} from "@shopify/react-form";
+import useReactRouter from "use-react-router";
+
+import {
+  Button,
+  Card,
+  FormLayout,
+  Layout,
+  Page,
+  Stack,
+  TextField
+} from "@shopify/polaris";
+import { useFirebase } from "../../../../components";
+
+export function Register() {
+  const { fields, submit, submitting, dirty, reset, submitErrors } = useForm({
+    fields: {
+      firstName: useField(""),
+      lastName: useField(""),
+      email: useField(""),
+      password: useField(""),
+      passwordConfirmation: useField("")
+    },
+    async onSubmit() {
+      return submitSuccess();
+    }
+  });
+  const { firstName, lastName, email, password, passwordConfirmation, phone, cardNumber } = fields;
+  const loading = submitting ? <p className="loading">loading...</p> : null;
+  const errors =
+    submitErrors.length > 0 ? (
+      <p className="error">{submitErrors.join(", ")}</p>
+    ) : null;
+
+  return (
+    <Page title="">
+      <img src="logo.png" height="100" />
+      <Layout>
+        <Layout.Section>
+          <Card primaryFooterAction={{ content: "Submit", onAction: submit }}>
+            <Card.Section>
+              <Stack vertical>
+                <FormLayout>
+                  <FormLayout.Group>
+                    {loading}
+                    {errors}
+                    <TextField name='fname' label="First" {...firstName} />
+                    <TextField label="Last" {...lastName} />
+                  </FormLayout.Group>
+                  <FormLayout.Group>
+                    <TextField label="Email" type="email" {...email} />
+                  </FormLayout.Group>
+                  <FormLayout.Group>
+                    <TextField label="Password" type="password" {...password} />
+                    <TextField
+                      label="Confirm Password"
+                      type="password"
+                      {...passwordConfirmation}
+                    />
+                    <TextField
+                      label=''
+                      name="cardnumber"
+                      {...cardNumber}
+                    />
+                    <TextField
+                      label=''
+                      name="phone"
+                      {...phone}
+                    />
+                  </FormLayout.Group>
+                </FormLayout>
+              </Stack>
+            </Card.Section>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+  return <>Register</>;
+}
