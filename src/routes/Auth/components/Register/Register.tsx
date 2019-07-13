53491decaa5f778cb940a184cd8e46b211e4a83b
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-07-13 17:31:07 -0400

    Formating and copy fixes

--- src/routes/Auth/components/Register/Register.tsx
+++ src/routes/Auth/components/Register/Register.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import {
   useForm,
@@ -18,20 +18,21 @@ import {
   TextField
 } from "@shopify/polaris";
 
-import {DetailsForm, useDetailFields} from './components'
+import { DetailsForm, useDetailFields } from "./components";
 
 export function Register() {
   const router = useReactRouter();
   const { fields, submit, submitting, dirty, reset, submitErrors } = useForm({
     fields: {
-      details: useDetailFields(),
+      details: useDetailFields()
     },
-    async onSubmit({details}) {
-      console.log(details)
+    async onSubmit({ details }) {
+      console.log(details);
+      setStep(step + 1);
       return submitSuccess();
     }
   });
-  const {details} = fields;
+  const { details } = fields;
   const [step, setStep] = useState(0);
   const loading = submitting ? <p className="loading">loading...</p> : null;
   const errors =
@@ -39,10 +40,10 @@ export function Register() {
       <p className="error">{submitErrors.join(", ")}</p>
     ) : null;
 
-  console.log(details)
+  console.log(details);
 
-  const formContent = ((step) => {
-    switch(step) {
+  const formContent = (step => {
+    switch (step) {
       case 0:
         return <DetailsForm {...details} />;
       case 1:
@@ -50,34 +51,32 @@ export function Register() {
       default:
         return <>Loading</>;
     }
-  })(step)
-
+  })(step);
 
   return (
     <Page title="">
-      <img src="logo.png" height="100" />
-      <Layout>
-        <Layout.Section>
-          <Card
-            primaryFooterAction={{
-              content: "Submit",
-              onAction: submit,
-              disabled: !dirty
-            }}
-            secondaryFooterAction={{
-              content: "Back",
-              onAction: () => {
-                submit();
-                router.history.goBack();
-              }
-            }}
-          >
-            <Card.Section>
-              {formContent}
-            </Card.Section>
-          </Card>
-        </Layout.Section>
-      </Layout>
+      <Page title="Details">
+        <Layout>
+          <Layout.Section>
+            <Card
+              primaryFooterAction={{
+                content: "Next",
+                onAction: submit,
+                disabled: !dirty
+              }}
+              secondaryFooterAction={{
+                content: "Back",
+                onAction: () => {
+                  submit();
+                  router.history.goBack();
+                }
+              }}
+            >
+              <Card.Section>{formContent}</Card.Section>
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </Page>
     </Page>
   );
 }
