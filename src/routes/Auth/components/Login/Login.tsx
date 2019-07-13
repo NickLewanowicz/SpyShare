65d939a07b779fa7a31152c072d05679cf23e49d
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-07-13 13:02:24 -0400

    Update Login to be pretty

--- src/routes/Auth/components/Login/Login.tsx
+++ src/routes/Auth/components/Login/Login.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import useReactRouter from 'use-react-router';
+import useReactRouter from "use-react-router";
 
-import {Button} from 'antd'
-import {useFirebase} from '../../../../components'
+import { Button, Card, Page, Layout, TopBar } from "@shopify/polaris";
+import { useFirebase } from "../../../../components";
 
 export function Login() {
-    const firebase = useFirebase()
-    const { history } = useReactRouter();
-    if (!firebase) {
-        return <>Error</>
+  const firebase = useFirebase();
+  const { history } = useReactRouter();
+  if (!firebase) {
+    return <>Error</>;
+  }
+  const doSignIn = async () => {
+    await firebase.doSignInWithGoogle();
+    if (firebase.auth.currentUser) {
+      history.push("/register");
     }
-    const doSignIn = async () => {
-        await firebase.doSignInWithGoogle()
-        if (firebase.auth.currentUser) {
-            history.push('/register')
-        }
-        firebase.auth.currentUser && console.log(firebase.auth.currentUser.photoURL)
-    }
+    firebase.auth.currentUser &&
+      console.log(firebase.auth.currentUser.photoURL);
+  };
 
-    const doSignout = async () => {
-         const response = await firebase.doSignOut()
-         console.log(response)
-    }
+  const doSignout = async () => {
+    const response = await firebase.doSignOut();
+    console.log(response);
+  };
 
-    const profile = firebase.auth.currentUser && firebase.auth.currentUser.photoURL;
-    return <>
-        Login
-        <Button onClick={doSignIn}/><Button onClick={doSignout}/>
-    </>
+  const profile =
+    firebase.auth.currentUser && firebase.auth.currentUser.photoURL;
+  return (
+    <Page title="">
+      <Layout>
+          <Card sectioned>
+              <p>Login or Signup with </p>
+          <Button onClick={doSignIn}>Login in with Google</Button>
+        <Button onClick={doSignout}>Sign out</Button>
+          </Card>
+        
+      </Layout>
+    </Page>
+  );
 }
